7cdee1f5630442237fce31eeedcfacfb7e2073c8
Bartek <bartlomiej.fr1@gmail.com>
2022-12-22 17:24:13 +0100

    machine typing finished

--- machine_typing/main.js
+++ machine_typing/main.js
@@ -1,30 +1,39 @@
 const box = document.querySelector(".typing");
 const text = [
-    "Hello, how are you ?",
-    "Nice to hear that you are fine!",
+    "Hello! ^How are you ?",
+    "Nice to hear that you are fine! ^What is you name?",
     "How can I help You ? ...",
 ];
 let wordIndex = 0;
 let textIndex = 0;
 let oldTime = 0;
-let speed = 75;
+const speed = 75;
+let activeDOMElement = box;
 
 const typing = (newTime) => {
-    if (wordIndex === text[textIndex].length) {
-        if (textIndex === text.length - 1) {
-            return;
-        }
-        return setTimeout(() => {
-            box.textContent = "";
-            textIndex++;
-            wordIndex = 0;
-            requestAnimationFrame(typing);
-        }, 2000);
-    }
     if (newTime - oldTime > speed) {
-        oldTime = newTime;
         const letter = text[textIndex].substr(wordIndex, 1);
-        box.textContent += letter;
+        if (wordIndex === text[textIndex].length) {
+            if (textIndex === text.length - 1) {
+                return;
+            }
+            return setTimeout(() => {
+                box.textContent = "";
+                textIndex++;
+                wordIndex = 0;
+                requestAnimationFrame(typing);
+            }, 2000);
+        } else if (wordIndex === 0 || letter === "^") {
+            const p = document.createElement("p");
+            box.appendChild(p);
+            activeDOMElement = p;
+        }
+
+        if (!(letter === "^")) {
+            activeDOMElement.textContent += letter;
+        }
+
+        oldTime = newTime;
         wordIndex++;
     }
     requestAnimationFrame(typing);
